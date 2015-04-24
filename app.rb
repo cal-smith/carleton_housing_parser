0d27f5540a4c620a1e45d685008bf81cf2750415
Callum Smith <callum1234moo@gmail.com>
2015-04-24 13:57:41 -0400

    this is actually the commit with json changes

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'sqlite3'
+require 'json'
 
 db = SQLite3::Database.new("housing.db")
 
 get '/?' do
+	db.results_as_hash = true
 	listing = db.execute("SELECT * FROM housing LIMIT 0, 100")
 	erb :index, :locals => {:listing => listing}
 end
@@ -12,24 +14,21 @@ end
 # price is upper price limit
 # furished is a simple true, false, or undefined option
 # type is the type of housing
-get '/limit/:zone/?:price?/?:furnished?/?:type?/?' do
+get '/limit/?.?:format?' do
 	sql = "select * from housing where "
 	sqlopts = []
 	array = []
 	unless params[:zone] == "all"
-		puts "no zone"
 		sqlopts << " zone in (#{params[:zone].split('&').collect{'?'}.join(',')}) "
 		array << params[:zone].split('&')
 	end
 
 	unless params[:type] == "all"
-		puts "no type"
 		sqlopts << " type in (#{params[:type].split('&').collect{'?'}.join(',')}) "
 		array << params[:type].split('&')
 	end
 
 	unless params[:furnished] == "all"
-		puts "no furns"
 		sqlopts << "furnished = ?"
 		array << [params[:furnished]]
 	end
@@ -45,9 +44,11 @@ get '/limit/:zone/?:price?/?:furnished?/?:type?/?' do
 	end
 
 	sql << " price <= ? order by price asc "
-	puts sql
-	puts sqlopts
-	puts array
+	db.results_as_hash = true
 	listing = db.execute(sql, array)
-	erb :index, :locals => {:listing => listing} 
+	if params[:format] == "json"
+		listing.to_json
+	else
+		erb :index, :locals => {:listing => listing}
+	end
 end
